0870c88237337cfdd664463f114709e3ce0716f2
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 12:43:06 -0700

    Add showAllUsers action to user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('user')
+  showAllUsers() {
+    this.userService.showAll();
+  }
+}
