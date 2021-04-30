ee79185ccb1ba77bb8035dba4d7324b9f9df48f8
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 12:22:21 -0700

    Add user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @Column({
+    type: 'text',
+    unique: true,
+  })
+  username: string;
+
+  @Column('text')
+  password: string;
+}
