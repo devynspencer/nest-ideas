021ed43f2e0a5c970ea443d7d133b49f098714a1
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 11:26:40 -0700

    Check for empty objects during validation

--- src/shared/validation.pipe.ts
+++ src/shared/validation.pipe.ts
@@ -10,6 +10,10 @@ import { validate } from 'class-validator';
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (value instanceof Object && this.isEmpty(value)) {
+      throw new BadRequestException('Validation failed: No body submitted');
+    }
+
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
@@ -28,5 +32,13 @@ export class ValidationPipe implements PipeTransform<any> {
     // eslint-disable-next-line
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
+  }
+
+  private isEmpty(value: any): boolean {
+    if (Object.keys(value).length > 0) {
+      return false;
+    }
+
+    return true;
   }
 }
