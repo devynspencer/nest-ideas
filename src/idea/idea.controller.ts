32efe1e825cba96cf80fd4b9f85d588962aad374
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 03:05:07 -0700

    Add placeholder CRUD methods for ideas controller

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('idea')
-export class IdeaController {}
+export class IdeaController {
+  @Get()
+  showAllIdeas() {
+
+  }
+
+  @Post()
+  createIdea() {
+
+  }
+
+  @Get(':id')
+  readIdea() {
+
+  }
+
+  @Put(':id')
+  updateIdea() {
+
+  }
+
+  @Delete(':id')
+  deleteidea() {
+
+  }
+}
