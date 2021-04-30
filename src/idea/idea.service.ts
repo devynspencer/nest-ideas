06c10692184f0c6aeb8158128a300f5ebea79380
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 03:26:37 -0700

    Inject idea entity into idea service

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IdeaEntity } from './idea.entity';
 
 @Injectable()
-export class IdeaService {}
+export class IdeaService {
+  constructor(
+    @InjectRepository(IdeaEntity)
+    private ideaRepository: Repository<IdeaEntity>,
+  ) {}
+}
