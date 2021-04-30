4d432b5fa846759911701088ea5d02bffe8659a6
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 15:15:20 -0700

    Enforce shape of user with a dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Logger, Post, UsePipes } from '@nestjs/common';
 import { ValidationPipe } from '../shared/validation.pipe';
+import { UserDto } from './user.dto';
 import { UserService } from './user.service';
 
 @Controller()
@@ -15,14 +16,14 @@ export class UserController {
 
   @Post('login')
   @UsePipes(new ValidationPipe())
-  login(@Body() data) {
+  login(@Body() data: UserDto) {
     this.logger.log(`User login: ${JSON.stringify(data)}`);
     return this.userService.login(data);
   }
 
   @Post('register')
   @UsePipes(new ValidationPipe())
-  register(@Body() data) {
+  register(@Body() data: UserDto) {
     this.logger.log(`User registered: ${JSON.stringify(data)}`);
     return this.userService.register(data);
   }
