72b40e96677fd3292987339825db8b347fb27567
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-04-30 19:50:48 -0700

    Add auth guard to idea controller

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -7,12 +7,14 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { IdeaService } from './idea.service';
 import { IdeaDto } from './idea.dto';
 import { ValidationPipe } from '../shared/validation.pipe';
 import { User } from '../user/user.decorator';
+import { AuthGuard } from '../shared/auth.guard';
 
 @Controller('api/ideas')
 export class IdeaController {
@@ -21,11 +23,13 @@ export class IdeaController {
   constructor(private ideaService: IdeaService) {}
 
   @Get()
+  @UseGuards(new AuthGuard())
   showAllIdeas(@User() user) {
     return this.ideaService.showAll();
   }
 
   @Post()
+  @UseGuards(new AuthGuard())
   @UsePipes(new ValidationPipe())
   createIdea(@Body() data: IdeaDto, @User() user) {
     this.logger.log(`Creating idea: ${JSON.stringify(data)}`);
@@ -33,11 +37,13 @@ export class IdeaController {
   }
 
   @Get(':id')
+  @UseGuards(new AuthGuard())
   readIdea(@Param('id') id: string, @User() user) {
     return this.ideaService.read(id);
   }
 
   @Put(':id')
+  @UseGuards(new AuthGuard())
   @UsePipes(new ValidationPipe())
   updateIdea(
     @Param('id') id: string,
@@ -49,6 +55,7 @@ export class IdeaController {
   }
 
   @Delete(':id')
+  @UseGuards(new AuthGuard())
   deleteidea(@Param('id') id: string, @User() user) {
     this.logger.log(`Deleting idea ${id}`);
     return this.ideaService.delete(id);
