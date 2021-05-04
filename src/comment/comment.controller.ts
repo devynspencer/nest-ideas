1b182bac30247bcfbb041fe3bb96ccce2b644aa7
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-03 19:46:32 -0700

    Chore - lint & format

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthGuard } from '../shared/auth.guard';
 import { User } from '../user/user.decorator';
 import { CommentDto } from './comment.dto';
@@ -9,30 +19,24 @@ export class CommentController {
   constructor(private commentService: CommentService) {}
 
   @Get('idea/:id')
-  showCommentsByIdea(@Param('id') idea: string) {
-
-  }
+  showCommentsByIdea(@Param('id') idea: string) {}
 
   @Get('user/:id')
-  showCommentsByUser(@Param('id') user: string) {
-
-  }
+  showCommentsByUser(@Param('id') user: string) {}
 
   @Get(':id')
-  showComment(@Param('id') id: string) {
-
-  }
+  showComment(@Param('id') id: string) {}
 
   @Post('idea/:id')
   @UseGuards(new AuthGuard())
   @UsePipes(new ValidationPipe())
-  createComment(@Param('id') idea: string, @User('id') user: string, @Body() data: CommentDto) {
-
-  }
+  createComment(
+    @Param('id') idea: string,
+    @User('id') user: string,
+    @Body() data: CommentDto,
+  ) {}
 
   @Delete('idea/:id')
   @UserGuards(new AuthGuard())
-  deleteComment(@Param('id') id: string, @User('id') user: string) {
-
-  }
+  deleteComment(@Param('id') id: string, @User('id') user: string) {}
 }
