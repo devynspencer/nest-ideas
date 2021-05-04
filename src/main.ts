ac5782b0246843238799fba35253c35e36b1311a
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 11:28:50 -0700

    Enable CORS for Angular default port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: 'http://localhost:4200' });
+
   await app.listen(port);
+
   Logger.log(`Starting server on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
